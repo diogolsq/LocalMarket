c73a1c73d8f16e4fcdf7fef14c9b64469571902b
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-01 18:19:10 -0300

    created logo, fixed the flashes partial, fixed navbar, fixed migration, CHANGED THE USER MODEL

--- db/migrate/20200601172308_create_users.rb
+++ db/migrate/20200601172308_create_users.rb
@@ -3,8 +3,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :name
       t.string :username
-      t.string :email
-      t.string :password
       t.string :address
       t.integer :cellphone
       t.string :role
