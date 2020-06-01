eaaf40e70fd66a8b3dc66cedc4fe87fa5e23c0e1
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-01 17:02:10 -0300

    corrected sintax mistakes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
-  has_many: order, through: :ordersToProduct
-  has_many: review, dependent: :destroy
-  validates: description, presence: true
-  validates: stock, presence: true, numericality: { only_integer: true }
+  has_many :order, through: :ordersToProduct
+  has_many :review, dependent: :destroy
+  validates :description, presence: true
+  validates :stock, presence: true, numericality: { only_integer: true }
 end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
   belongs_to :product
   belongs_to :user
-  validates: description, presence: true, length: { minimum: 5}
+  validates :description, presence: true, length: { minimum: 5}
 end
