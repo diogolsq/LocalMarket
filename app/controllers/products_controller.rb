128f1ae4cedfb09ddab703c30d48170fc357b639
Nicole Ochoa <nochoa91@gmail.com>
2020-06-01 21:24:49 -0500

    Updated products controller with edit, update and destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,14 +10,30 @@ class ProductsController < ApplicationController
 
   def new
     @product = Product.new
-
   end
 
   def create
-    @product = Product.new(params[:product])
-    @product.save
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
 
-    redirect_to product_path(@product)
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy product_path(@product)
   end
 
   private
