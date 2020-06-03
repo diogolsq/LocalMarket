c278bfe85d2321e480848529831184040ac7d21a
Nicole Ochoa <nochoa91@gmail.com>
2020-06-02 21:22:37 -0500

    finished show add to cart stil missing

--- app/controllers/orders_to_products_controller.rb
+++ app/controllers/orders_to_products_controller.rb
@@ -1,6 +1,3 @@
 class OrdersToProductsController < ApplicationController
 
-  def new
-    @orders_to_product = OrdersToProducts.new
-  end
 end
--- app/models/orders_to_product.rb
+++ app/models/orders_to_product.rb
@@ -1,6 +1,5 @@
-class OrdersToProducts < ApplicationRecord
+class OrdersToProduct < ApplicationRecord
   belongs_to :product
   belongs_to :order
   validates :quantity, :unit_price, presence: true
-
 end
