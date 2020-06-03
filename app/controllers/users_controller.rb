b29a132d5aefc4c5a8bcde627f86f21680be5cd3
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-03 06:21:00 -0300

    generate the users controller, the use shows pages, created the user policy to handle who can access that page where you can edit your user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
-  def show
+  before_action :find_user, only: [:show]
 
+  def show
+  authorize @user
   end
 
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
