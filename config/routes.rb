ff0088040b8e692884a7db18e8ff0ba74a5476e1
Nicole Ochoa <nochoa91@gmail.com>
2020-06-02 20:03:21 -0500

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products do
+  resources :products, only: [:index, :show] do
     resources :reviews, only: [ :new, :create ]
   end
-  resources :reviews, only: [ :destroy ]
+  resources :users, only: [ :show ] do
+    resources :products, only: [:index, :new, :create, :update, :destroy]
+  end
 end
