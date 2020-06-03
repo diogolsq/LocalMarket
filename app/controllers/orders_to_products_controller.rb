f40e3b34de38839736a831ff56706988f6689bff
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-03 20:08:54 -0300

    routes

--- app/controllers/orders_to_products_controller.rb
+++ app/controllers/orders_to_products_controller.rb
@@ -1,7 +1,6 @@
 class OrdersToProductsController < ApplicationController
 
   def show
-    @orderstoproducts = @OrdersToProducts.find(params[:id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
+
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :show, :create] do
+  resources :products do
     resources :reviews, only: [ :new, :create ]
   end
   resources :users, only: [ :show ] do
-    resources :products, only: [:index, :new, :create, :update, :destroy]
+    resources :orders, only: [ :show ]
   end
-  resources :orders_to_products, only: [:show]
-
 end
