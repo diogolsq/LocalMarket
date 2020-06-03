56768fb6be075c596cac8ab39bf928831213ab33
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-03 15:20:12 -0300

    fixing Js for every page putting a condition in the Type.Js logic, fixed the initmapbox logic to work with an array, fixed the show page

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,11 +1,15 @@
 import Typed from 'typed.js';
 
+
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["access to local business", "near you"],
-    typeSpeed: 50,
-    loop: true
-  });
+  if (document.getElementById('banner-typed-text')) {
+
+    new Typed('#banner-typed-text', {
+      strings: ["access to local business", "near you"],
+      typeSpeed: 50,
+      loop: true
+    });
+  }
 }
 
 export { loadDynamicBannerText };
