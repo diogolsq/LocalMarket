d00a71703ee213469b33488838479fa09a6e1c6f
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-03 11:56:23 -0300

    fixin the routes to have update and destroy in the product route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products, only: [:index, :show, :create] do
+  resources :products, only: [:index, :show, :create, :update, :destroy] do
     resources :reviews, only: [ :new, :create ]
   end
   resources :users, only: [ :show ] do
