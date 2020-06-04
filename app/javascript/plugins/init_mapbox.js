584a2bbe21c6e7d195d83ed46ce7fdd9e2b5250d
Diogo Queiroz <diogolsq@poli.ufrj.br>
2020-06-04 12:33:33 -0300

    fixing the map tha was breaking, good to go now, info window and our logo being the mark of the map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -13,14 +13,19 @@ const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow); // add this
 
-    new mapboxgl.Marker()
+    const element = document.createElement('div');
+    element.className = 'marker';
+    element.style.backgroundImage = `url('${marker.image_url}')`;
+    element.style.backgroundSize = 'contain';
+    element.style.width = '25px';
+    element.style.height = '25px';
+
+    new mapboxgl.Marker(element)
       .setLngLat([ marker.lng, marker.lat ])
       .setPopup(popup) // add this
       .addTo(map);
   });
 };
-
-
 
 
 
@@ -37,13 +42,9 @@ const initMapbox = () => {
     });
 
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([ marker.lng, marker.lat ])
-        .addTo(map);
-    });
+    addMarkersToMap(map, markers);
 
-    fitMapToMarkers(map, marker);
+    fitMapToMarkers(map, markers);
 
   }
 };
