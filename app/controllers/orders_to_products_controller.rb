db082db3adab9c113642e077bc675234675020bd
Nicole Ochoa <nochoa91@gmail.com>
2020-06-03 19:04:52 -0500

    Added create in OrdersToProductsController

--- app/controllers/orders_to_products_controller.rb
+++ app/controllers/orders_to_products_controller.rb
@@ -1,3 +1,29 @@
 class OrdersToProductsController < ApplicationController
 
+  def create
+    product = Product.find(params[:product_id])
+
+    open_order = current_user.orders.find_by(status: "open")
+    open_order ||= current_user.orders.create(status: "open")
+    new_order = OrdersToProduct.new(orders_to_product_params)
+    new_order.unit_price = product.price
+    new_order.order = open_order
+
+    new_order.save
+
+    redirect_to new_order.product, notice: "Succesfully added to cart"
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def orders_to_product_params
+    params.require(:orders_to_product).permit(:quantity)
+  end
+
 end
