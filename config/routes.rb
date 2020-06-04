7044a1ad3fb6a528c5b193821b784c3518fd60ec
Marianne Aguiar <mariannehaguiar@gmail.com>
2020-06-04 10:47:05 -0300

    fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :products do
     resources :reviews, only: [ :new, :create ]
   end
-  resources :users, only: [ :show ] do
-    resources :orders, only: [ :show ]
-  end
+  resources :users, only: [ :show ]
+  resources :orders, only: [ :show ]
 end
